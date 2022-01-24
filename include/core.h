f33a12b552a6d9fed536cdce0c7bef10dc7402b0
mateuszokroj1 <mateuszokroj1@gmail.com>
2022-01-24 18:53:23 +0100

    Fixed includes path.

--- include/core.h
+++ include/core.h
@@ -1,6 +1,8 @@
-#include <intrin.h>
-#include <mmintrin.h>
-#include <xmmintrin.h>
+#ifdef WIN32
+	#include <intrin.h>
+#elif defined(__i386__) || defined(__x86_64__)
+	#include <x86intrin.h>
+#endif
 
 #ifndef CORE_H
 #define CORE_H
