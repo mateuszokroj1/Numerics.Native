7e1e18c5549b83ecc06c20ab6e9b2051b36689f5
mateuszokroj1 <mateuszokroj1@gmail.com>
2022-01-25 00:02:13 +0100

    First core operations.

--- include/core.h
+++ include/core.h
@@ -5,6 +5,30 @@
 #ifndef CORE_H
 #define CORE_H
 
+float __vectorcall sum(float, float);
+double __vectorcall sum(double, double);
+__m128 __vectorcall sum(__m128, __m128);
+__m128d __vectorcall sum(__m128d, __m128d);
+__m256 __vectorcall sum(__m256, __m256);
+__m256d __vectorcall sum(__m256d, __m256d);
 
+__m128 __vectorcall horizontal_sum(__m128, __m128);
+__m128d __vectorcall horizontal_sum(__m128d, __m128d);
+__m256 __vectorcall horizontal_sum(__m256, __m256);
+__m256d __vectorcall horizontal_sum(__m256d, __m256d);
+
+float __vectorcall mul(float, float);
+double __vectorcall mul(double, double);
+__m128 __vectorcall mul(__m128, __m128);
+__m128d __vectorcall mul(__m128d, __m128d);
+__m256 __vectorcall mul(__m256, __m256);
+__m256d __vectorcall mul(__m256d, __m256d);
+
+float __vectorcall div(float, float);
+double __vectorcall div(double, double);
+__m128 __vectorcall div(__m128, __m128);
+__m128d __vectorcall div(__m128d, __m128d);
+__m256 __vectorcall div(__m256, __m256);
+__m256d __vectorcall div(__m256d, __m256d);
 
 #endif
--- src/core.cpp
+++ src/core.cpp
@@ -1,1 +1,10 @@
 #include "core.h"
+
+float __vectorcall sum(float a, float b)
+{
+	__m128 xmm0, xmm1;
+	xmm0.m128_f32[0] = a;
+	xmm1.m128_f32[0] = b;
+	return _mm_add_ss(xmm0, xmm1).m128_f32[0];
+}
+
