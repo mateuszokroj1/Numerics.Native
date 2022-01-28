d14885c91867c0263eb7781ccf5f9ca7efbfc2a2
mateuszokroj1 <mateuszokroj1@gmail.com>
2022-01-28 14:49:10 +0100

    Removed calling conventions. Added extern C.

--- include/core.h
+++ include/core.h
@@ -5,30 +5,33 @@
 #ifndef CORE_H
 #define CORE_H
 
-float add(float, float);
-double add(double, double);
-__m128 __vectorcall add(__m128, __m128);
-__m128d __vectorcall add(__m128d, __m128d);
-__m256 __vectorcall add(__m256, __m256);
-__m256d __vectorcall add(__m256d, __m256d);
+extern "C"
+{
+	float add(float, float);
+	double add(double, double);
+	__m128 add(__m128, __m128);
+	__m128d add(__m128d, __m128d);
+	__m256 add(__m256, __m256);
+	__m256d add(__m256d, __m256d);
 
-__m128 __vectorcall horizontal_add(__m128, __m128);
-__m128d __vectorcall horizontal_add(__m128d, __m128d);
-__m256 __vectorcall horizontal_add(__m256, __m256);
-__m256d __vectorcall horizontal_add(__m256d, __m256d);
+	__m128 horizontal_add(__m128, __m128);
+	__m128d horizontal_add(__m128d, __m128d);
+	__m256 horizontal_add(__m256, __m256);
+	__m256d horizontal_add(__m256d, __m256d);
 
-float __vectorcall mul(float, float);
-double __vectorcall mul(double, double);
-__m128 __vectorcall mul(__m128, __m128);
-__m128d __vectorcall mul(__m128d, __m128d);
-__m256 __vectorcall mul(__m256, __m256);
-__m256d __vectorcall mul(__m256d, __m256d);
+	float multiply(float, float);
+	double multiply(double, double);
+	__m128 multiply(__m128, __m128);
+	__m128d multiply(__m128d, __m128d);
+	__m256 multiply(__m256, __m256);
+	__m256d multiply(__m256d, __m256d);
 
-float __vectorcall div(float, float);
-double __vectorcall div(double, double);
-__m128 __vectorcall div(__m128, __m128);
-__m128d __vectorcall div(__m128d, __m128d);
-__m256 __vectorcall div(__m256, __m256);
-__m256d __vectorcall div(__m256d, __m256d);
+	float divide(float, float);
+	double divide(double, double);
+	__m128 divide(__m128, __m128);
+	__m128d divide(__m128d, __m128d);
+	__m256 divide(__m256, __m256);
+	__m256d divide(__m256d, __m256d);
+}
 
 #endif
