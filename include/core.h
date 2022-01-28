68a86ad2a8116dd3b794b9b338d2c1d7c496c8ff
mateuszokroj1 <mateuszokroj1@gmail.com>
2022-01-28 19:25:11 +0100

    Reorganized functions for dllexport.

--- include/core.h
+++ include/core.h
@@ -1,37 +1,33 @@
 #include <intrin.h>
-	#include <x86intrin.h>
-#endif
+#include "Numerics_Native_Export.h"
 
 #ifndef CORE_H
 #define CORE_H
 
-extern "C"
-{
-	float add(float, float);
-	double add(double, double);
-	__m128 add(__m128, __m128);
-	__m128d add(__m128d, __m128d);
-	__m256 add(__m256, __m256);
-	__m256d add(__m256d, __m256d);
+NUMERICS_NATIVE_EXPORT float add(float, float);
+NUMERICS_NATIVE_EXPORT double add(double, double);
+NUMERICS_NATIVE_EXPORT __m128 add(__m128, __m128);
+NUMERICS_NATIVE_EXPORT __m128d add(__m128d, __m128d);
+NUMERICS_NATIVE_EXPORT __m256 add(__m256, __m256);
+NUMERICS_NATIVE_EXPORT __m256d add(__m256d, __m256d);
 
-	__m128 horizontal_add(__m128, __m128);
-	__m128d horizontal_add(__m128d, __m128d);
-	__m256 horizontal_add(__m256, __m256);
-	__m256d horizontal_add(__m256d, __m256d);
+NUMERICS_NATIVE_EXPORT __m128 horizontal_add(__m128, __m128);
+NUMERICS_NATIVE_EXPORT __m128d horizontal_add(__m128d, __m128d);
+NUMERICS_NATIVE_EXPORT __m256 horizontal_add(__m256, __m256);
+NUMERICS_NATIVE_EXPORT __m256d horizontal_add(__m256d, __m256d);
 
-	float multiply(float, float);
-	double multiply(double, double);
-	__m128 multiply(__m128, __m128);
-	__m128d multiply(__m128d, __m128d);
-	__m256 multiply(__m256, __m256);
-	__m256d multiply(__m256d, __m256d);
+NUMERICS_NATIVE_EXPORT float multiply(float, float);
+NUMERICS_NATIVE_EXPORT double multiply(double, double);
+NUMERICS_NATIVE_EXPORT __m128 multiply(__m128, __m128);
+NUMERICS_NATIVE_EXPORT __m128d multiply(__m128d, __m128d);
+NUMERICS_NATIVE_EXPORT __m256 multiply(__m256, __m256);
+NUMERICS_NATIVE_EXPORT __m256d multiply(__m256d, __m256d);
 
-	float divide(float, float);
-	double divide(double, double);
-	__m128 divide(__m128, __m128);
-	__m128d divide(__m128d, __m128d);
-	__m256 divide(__m256, __m256);
-	__m256d divide(__m256d, __m256d);
-}
+NUMERICS_NATIVE_EXPORT float divide(float, float);
+NUMERICS_NATIVE_EXPORT double divide(double, double);
+NUMERICS_NATIVE_EXPORT __m128 divide(__m128, __m128);
+NUMERICS_NATIVE_EXPORT __m128d divide(__m128d, __m128d);
+NUMERICS_NATIVE_EXPORT __m256 divide(__m256, __m256);
+NUMERICS_NATIVE_EXPORT __m256d divide(__m256d, __m256d);
 
 #endif
--- src/core.cpp
+++ src/core.cpp
@@ -1,6 +1,6 @@
 #include "core.h"
 
-extern "C" float add(float a, float b)
+NUMERICS_NATIVE_EXPORT float add(float a, float b)
 {
 	__m128 xmm0, xmm1;
 	xmm0.m128_f32[0] = a;
@@ -8,10 +8,34 @@ extern "C" float add(float a, float b)
 	return _mm_add_ss(xmm0, xmm1).m128_f32[0];
 }
 
-extern "C" double add(double a, double b)
+NUMERICS_NATIVE_EXPORT double add(double a, double b)
 {
 	__m128d xmm0, xmm1;
 	xmm0.m128d_f64[0] = a;
 	xmm1.m128d_f64[0] = b;
 	return _mm_add_sd(xmm0, xmm1).m128d_f64[0];
 }
+
+NUMERICS_NATIVE_EXPORT __m128 add(__m128, __m128) {}
+NUMERICS_NATIVE_EXPORT __m128d add(__m128d, __m128d){}
+NUMERICS_NATIVE_EXPORT __m256 add(__m256, __m256){}
+NUMERICS_NATIVE_EXPORT __m256d add(__m256d, __m256d){}
+
+NUMERICS_NATIVE_EXPORT __m128 horizontal_add(__m128, __m128){}
+NUMERICS_NATIVE_EXPORT __m128d horizontal_add(__m128d, __m128d){}
+NUMERICS_NATIVE_EXPORT __m256 horizontal_add(__m256, __m256){}
+NUMERICS_NATIVE_EXPORT __m256d horizontal_add(__m256d, __m256d){}
+
+NUMERICS_NATIVE_EXPORT float multiply(float, float){}
+NUMERICS_NATIVE_EXPORT double multiply(double, double){}
+NUMERICS_NATIVE_EXPORT __m128 multiply(__m128, __m128){}
+NUMERICS_NATIVE_EXPORT __m128d multiply(__m128d, __m128d){}
+NUMERICS_NATIVE_EXPORT __m256 multiply(__m256, __m256){}
+NUMERICS_NATIVE_EXPORT __m256d multiply(__m256d, __m256d){}
+
+NUMERICS_NATIVE_EXPORT float divide(float, float){}
+NUMERICS_NATIVE_EXPORT double divide(double, double){}
+NUMERICS_NATIVE_EXPORT __m128 divide(__m128, __m128){}
+NUMERICS_NATIVE_EXPORT __m128d divide(__m128d, __m128d){}
+NUMERICS_NATIVE_EXPORT __m256 divide(__m256, __m256){}
+NUMERICS_NATIVE_EXPORT __m256d divide(__m256d, __m256d){}
